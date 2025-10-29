7d38a81396aa4e136a1d9c4793ed61890e12285b
emuenu <107606113+emuenu@users.noreply.github.com>
2025-10-30 01:01:41 +0900

    feat: mini game

--- React_app/src/components/App.jsx
+++ React_app/src/components/App.jsx
@@ -4,6 +4,7 @@ import '../styles/App.css'
 
 import RegisterUsersPage from './RegisterUsersPage';
 import RankingPage from './RankingPage';
+import PlayingPage from "./Playing";
 
 function Nav() {
   const location = useLocation();
@@ -28,6 +29,7 @@ function App() {
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/register" element={<RegisterUsersPage />} />
         <Route path="/ranking" element={<RankingPage />} />
+        <Route path="/playing" element={<PlayingPage />} />
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </Router>
